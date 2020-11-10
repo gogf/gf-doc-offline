a948eb2ae74a6f9e670fa52995e443ec405e4009
hailaz <739476267@qq.com>
2020-11-10 20:20:23 +0800

    update to v1.14

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # gf-doc-offline
+
 gf-doc项目的离线版
 
 # 使用方法
+
 下载解压后，按系统类型操作，访问http://localhost:8080
 
-端口可在config.toml中修改
+端口可在config/config.toml中修改
 
-## [下载地址](https://www.lanzous.com/b00n7p8sh)
+## 下载地址
+
+[新版](https://github.com/hailaz/gf-doc-offline/releases)
+
+[旧版](https://www.lanzous.com/b00n7p8sh)
 
 ##  windows
+
 双击run.bat
 
 或执行gf-doc-offline
 
 ## linux
+
 执行gf-doc-offline
 
 ## macOS
+
 执行gf-doc-offline
--- build.bat
+++ build.bat
@@ -1,21 +1,29 @@
 ::用于构建项目所有文件
-@echo off 
-SET docPath=%gopath%\src\github.com\gogf\gf-doc
-SET homePath=%gopath%\src\github.com\gogf\gf-home
+@echo off
+SET basePath=%cd%
 SET projectName=gf-doc-offline
-
-if exist %docPath% (
-    echo %docPath%
-) else (
-    echo path "%docPath%" not exist
-    goto stop
+echo basePath: %basePath%
+SET tempPath=%basePath%\build_temp
+echo tempPath: %tempPath%
+if not exist %tempPath% (
+    mkdir %tempPath%
 )
 
-if exist %homePath% (
-    echo %homePath%
-) else (
+SET docPath=%tempPath%\gf-doc
+SET homePath=%tempPath%\gf-home
+echo docPath: %docPath%
+
+if not exist %docPath% (
+    echo path "%docPath%" not exist
+    cd %tempPath%
+    git clone https://github.com/gogf/gf-doc.git
+)
+
+echo homePath: %homePath%
+if not exist %homePath% (
     echo path "%homePath%" not exist
-    goto stop
+    cd %tempPath%
+    git clone https://github.com/gogf/gf-home.git
 )
 
 ::复制文档文件
@@ -28,56 +36,82 @@ md public
 xcopy %homePath%\public public /s /e /y
 
 SET CGO_ENABLED=0
+SET GO111MODULE=on
 SET packFiles=config gf-doc public template %projectName%
+
+@REM start build
+
 ::win32
+echo "build win32"
+cd %homePath%
 SET GOOS=windows
 SET GOARCH=386
-SET binName=%projectName%-%GOOS%%GOARCH%.exe
+SET binName=%homePath%\%projectName%-%GOOS%%GOARCH%.exe
 go build -o %binName% %homePath%\main.go
-ren %binName% %projectName%.exe
+cd %basePath%
+copy %binName% %projectName%.exe
 7z.exe a %projectName%-%GOOS%%GOARCH%.zip %packFiles%.exe run.bat
 del %projectName%.exe /Q
+del %binName% /Q
 
 ::linux32
+echo "build linux32"
+cd %homePath%
 SET GOOS=linux
 SET GOARCH=386
-SET binName=%projectName%-%GOOS%%GOARCH%
+SET binName=%homePath%\%projectName%-%GOOS%%GOARCH%
 go build -o %binName% %homePath%\main.go
-ren %binName% %projectName%
+cd %basePath%
+copy %binName% %projectName%
 7z.exe a %projectName%-%GOOS%%GOARCH%.zip %packFiles%
 del %projectName% /Q
+del %binName% /Q
 
 ::linux64
+echo "build linux64"
+cd %homePath%
 SET GOOS=linux
 SET GOARCH=amd64
-SET binName=%projectName%-%GOOS%%GOARCH%
+SET binName=%homePath%\%projectName%-%GOOS%%GOARCH%
 go build -o %binName% %homePath%\main.go
-ren %binName% %projectName%
+cd %basePath%
+copy %binName% %projectName%
 7z.exe a %projectName%-%GOOS%%GOARCH%.zip %packFiles%
 del %projectName% /Q
+del %binName% /Q
 
-::darwin32
-SET GOOS=darwin
-SET GOARCH=386
-SET binName=%projectName%-%GOOS%%GOARCH%
-go build -o %binName% %homePath%\main.go
-ren %binName% %projectName%
-7z.exe a %projectName%-%GOOS%%GOARCH%.zip %packFiles%
-del %projectName% /Q
+@REM cmd/go: unsupported GOOS/GOARCH pair darwin/386
+@REM ::darwin32
+@REM echo "build darwin32"
+@REM cd %homePath%
+@REM SET GOOS=darwin
+@REM SET GOARCH=386
+@REM SET binName=%homePath%\%projectName%-%GOOS%%GOARCH%
+@REM echo go build -o %binName% %homePath%\main.go
+@REM cd %basePath%
+@REM copy %binName% %projectName%
+@REM 7z.exe a %projectName%-%GOOS%%GOARCH%.zip %packFiles%
+@REM del %projectName% /Q
+@REM del %binName% /Q
 
 ::darwin64
+echo "build darwin64"
+cd %homePath%
 SET GOOS=darwin
 SET GOARCH=amd64
-SET binName=%projectName%-%GOOS%%GOARCH%
+SET binName=%homePath%\%projectName%-%GOOS%%GOARCH%
 go build -o %binName% %homePath%\main.go
-ren %binName% %projectName%
+cd %basePath%
+copy %binName% %projectName%
 7z.exe a %projectName%-%GOOS%%GOARCH%.zip %packFiles%
 del %projectName% /Q
-
+del %binName% /Q
 
 REM GOOS：目标平台的操作系统（darwin、freebsd、linux、windows） 
 REM GOARCH：目标平台的体系架构（386、amd64、arm） 
 REM 交叉编译不支持 CGO 所以要禁用它
+
+@REM goto stop
 
 :stop
 pause
